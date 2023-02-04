433b0c76db2e43a912b6c79ad798edcec3b5d7fe
SheilaFundora <sheilafundora04@gamil.com>
2023-02-04 12:27:26 -0500

    backend finish

--- controllers/notes.js
+++ controllers/notes.js
@@ -6,14 +6,12 @@ const Process = require("process");
 const getNotes =  async (req, res = response) => {
 
     const payload = jwt.verify(
-        req.body.token,
+        req.header('x-token'),
         Process.env.SECRET_JWT
     )
 
     user_id = payload.uid;
-
-    console.log(payload)
-
+    
     try{
         const note =  await Note.find({user: Object(user_id)});
 
@@ -43,7 +41,7 @@ const createNotes = async (req, res = response) => {
         const note = new Note( req.body);
 
         const payload = jwt.verify(
-            req.body.token,
+            req.header('x-token'),
             Process.env.SECRET_JWT
         )
 
@@ -65,63 +63,17 @@ const createNotes = async (req, res = response) => {
     }
 }
 
-const deleteNotes =  async (req, res = response) => {
-
-    const noteId = "63ddd8dee8720c3898d2b0ca";
-
-    console.log(noteId)
-
-    try{
-        const event =  await Note.findById(noteId);
-
-        if( !event ){
-            return res.status(400).json({
-                ok: false,
-                sms: 'El eventpo no existe por esa id'
-            })
-        }
-        const payload = jwt.verify(
-            req.header('x-token'),
-            Process.env.SECRET_JWT
-        )
-
-        user_id = payload.uid;
-
-        if( event.user.toString() !== user_id ){
-            return res.status(401).json({
-                ok: false,
-                sms: 'No tiene permisos para eliminar este evento'
-            })
-
-        }
-
-        await Event.findByIdAndDelete(eventId);//resive el id
-
-        res.status(201).json({
-            ok: true,
-            sms: 'El evento se ha eliminado exitosamente'
-        })
-
-    }catch(error){
-        console.log(error);
-        res.status(500).json({
-            ok: false,
-            sms: 'Por favor hable con el administrador'
-        })
-    }
-}
-
 const updateNotes =  async (req, res = response) => {
 
-    const eventId = req.params.id; //asi m quedo con lo q sige en la url, con el id
+    const noteId = req.params.id;
 
     try{
-        const event = await Event.findById(eventId)
+        const note = await Note.findById(noteId)
 
-        if( !event ){
+        if( !note ){
             return res.status(400).json({
                 ok: false,
-                sms: 'El eventpo no existe por esa id'
+                sms: 'La nota no existe por esa id'
             })
         }
 
@@ -132,30 +84,18 @@ const updateNotes =  async (req, res = response) => {
 
         user_id = payload.uid;
 
-        if( event.user.toString() !== user_id ){ //si esto pasa es q hay una persona q quiere editar el evento sin permiso ya q no
 
-            // la q lo crop, no su evento
-            return res.status(401).json({
-                ok: false,
-                sms: 'No tiene permisos para actualizar este evento'
-            })
-        }
-
-        const newEvent = {
+        const newNote = {
             ...req.body,
-            user: event.user.toString()
+            user: note.user.toString()
         }
 
+        const noteUpdated = await Note.findByIdAndUpdate( noteId, newNote, {new:true} );
 
-        //o sea aqui le decimos q busque el elemento por el id y lo actualice, le pasamos el id del evento que queremos
-        // actaluzar y el nuev cntenido
-        const eventUpdated = await Event.findByIdAndUpdate( eventId, newEvent, {new:true} );
-
-        console.log(newEvent)
 
         res.status(201).json({
             ok: true,
-            eventUpdated
+            noteUpdated
 
         })
 
@@ -167,6 +107,37 @@ const updateNotes =  async (req, res = response) => {
         })
     }
 }
+
+const deleteNotes =  async (req, res = response) => {
+
+    const noteId = req.params.id;
+
+    try{
+        const note =  await Note.findById(noteId);
+
+        if( !note ){
+            return res.status(400).json({
+                ok: false,
+                sms: 'La nota no existe por esa id'
+            })
+        }
+
+        await Note.findByIdAndDelete(noteId);//resive el id
+
+        res.status(201).json({
+            ok: true,
+            sms: 'La nota se ha eliminado exitosamente'
+        })
+
+    }catch(error){
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            sms: 'Por favor hable con el administrador'
+        })
+    }
+}
+
 
 module.exports = {
     getNotes,
